1e9a410856c8d6ff0418bda319b06164ff176234
Aracki <aracki.ivan@gmail.com>
2018-11-01 13:58:22 +0100

    Add UTF-8 encoding

--- src/main/java/com/ibm/ibm/Receiver.java
+++ src/main/java/com/ibm/ibm/Receiver.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,15 +35,25 @@ public class Receiver {
             System.out.println("Please specify correct file location in application.properties file.");
             return;
         }
-        System.out.println("Received: " + message);
+
+        String encodedMsg = null;
+        try {
+            encodedMsg = new String(message.getBytes(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("UnsupportedEncodingException: " + e.getMessage());
+            return;
+        }
+
+        System.out.println("Received message: " + message);
+        System.out.println("Encoded message:" + encodedMsg);
 
         Path filePath = Paths.get(fileLocation + LocalDate.now());
         try {
             if (Files.notExists(filePath)) {
                 System.out.println("Writing message to a file...");
-                writeToFile(filePath, message, StandardOpenOption.CREATE);
+                writeToFile(filePath, encodedMsg, StandardOpenOption.CREATE);
             } else {
-                writeToFile(filePath, message, StandardOpenOption.APPEND);
+                writeToFile(filePath, encodedMsg, StandardOpenOption.APPEND);
             }
         } catch (IOException e) {
             System.out.println("Error occurred: " + e.getMessage());
